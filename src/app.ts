7a9d6c4066933a5edc5bbdb76d697fe604b1c848
Abbas Lamouri <abbaslamouri@yrlus.com>
2022-05-20 06:51:40 -0400

    M

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,23 @@
 import os from 'os'
 import express from 'express'
-// import cors from 'cors'
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
 // import AppError from './utils/AppError'
 // import { createClient } from 'redis'
 // import connectRedis from 'connect-redis'
 // import session from 'express-session'
-// import errorHandler from './utils/errorHandler'
+import errorHandler from './utils/errorHandler'
 // import config from './config/default'
 // import categoryRouter from './routes/v1/categories'
-// import authRouter from './routes/v1/auth'
-// import userRouter from './routes/v1/users'
+import authRouter from './routes/v1/auth'
+import userRouter from './routes/v1/users'
+import { IUser } from './models/user'
+
+declare module 'express' {
+  interface Request {
+    user?: IUser
+  }
+}
 
 // declare module 'express-session' {
 //   export interface SessionData {
@@ -24,8 +32,8 @@ import express from 'express'
 // })
 
 const app = express()
-// app.set('trust proxy', true)
-// app.use(cors({}))
+app.set('trust proxy', true)
+app.use(cors({}))
 
 // app.use(
 //   session({
@@ -37,8 +45,9 @@ const app = express()
 //   })
 // )
 
-// app.use(express.json({ limit: '1000kb' }))
-// app.use(express.urlencoded({ extended: true }))
+app.use(express.json({ limit: '1000kb' }))
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
 
 app.get('/api/v1/ping', async (req, res) => {
   console.log(`Sending response from container ${os.hostname()}`)
@@ -50,12 +59,12 @@ app.get('/api/v1/ping', async (req, res) => {
   })
 })
 // app.use('/api/v1/categories', categoryRouter)
-// app.use('/api/v1/auth', authRouter)
-// app.use('/api/v1/users', userRouter)
+app.use('/api/v1/auth', authRouter)
+app.use('/api/v1/users', userRouter)
 // app.all('*', async (req: Request, res: Response, next) => {
 //   next(new AppError(`Can't find ${req.originalUrl} on this server`, 404))
 // })
 
-// app.use(errorHandler)
+app.use(errorHandler)
 
 export default app
--- src/server.ts
+++ src/server.ts
@@ -1,46 +1,45 @@
-// import mongoose from 'mongoose'
+import mongoose from 'mongoose'
 import colors from 'colors'
 // import dotenv from 'dotenv'
-// dotenv.config()
+// dotenv.process.env()
 
 import app from './app'
-// import config from './config/default'
+// import process.env from './process.env/default'
 
-// const dbConnect = async () => {
-//   console.log('DBURI', config.DB_URI)
-//   console.log('ENV', config.NODE_ENV)
-//   console.log('JWT SECRET', config.JWT_SECRET)
-//   try {
-//     if (!config.JWT_SECRET) throw new Error('Environment variable JWT_SECRET is not defined')
-//     if (!config.DB_URI) throw new Error('Environment variable DB_URI is not defined')
-//     await mongoose.connect(config.DB_URI)
-//     console.log(colors.magenta.bold(`Database connection succesfull`))
-//     app.listen(config.PORT, () => {
-//       console.log(colors.cyan.bold(`server running on port ${config.PORT}...`))
-//     })
-//   } catch (err) {
-//     console.log(colors.red.bold(`Mongo DB Error ${err}`))
-//     // console.log(colors.red.bold(`Mongo DB Error Message ${err.message}`))
-//   }
-// }
+const dbConnect = async () => {
+  const dbUrl = `mongodb://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@${process.env.MONGO_DB_IP}:${process.env.MONGO_DB_PORT}`
+  // console.log('DBURI', process.env.DB_URI)
+  // console.log('ENV', process.env.NODE_ENV)
+  // console.log('JWT SECRET', process.env.JWT_SECRET)
+  try {
+    // if (!process.env.JWT_SECRET) throw new Error('Environment variable JWT_SECRET is not defined')
+    // if (!process.env.DB_URI) throw new Error('Environment variable DB_URI is not defined')
+    await mongoose.connect(dbUrl)
+    console.log(colors.magenta.bold(`Database connection succesfull`))
+    app.listen(process.env.PORT, () => {
+      console.log(colors.cyan.bold(`server running on port ${process.env.PORT || 8080}...`))
+    })
+  } catch (err) {
+    console.log(colors.red.bold(`Mongo DB Error ${err}`))
+    // console.log(colors.red.bold(`Mongo DB Error Message ${err.message}`))
+  }
+}
 
-// dbConnect()
-
-// const dbUrl = `mongodb://${config.DB_USER}:${config.DB_PASSWORD}@${config.DB_IP}:${config.DB_PORT}`
+dbConnect()
 
 // mongoose
-//   .connect(dbUrl, { dbName: config.DB_NAME })
+//   .connect(dbUrl, { dbName: process.env.DB_NAME })
 //   .then(() => {
 //     console.log(colors.magenta.bold(`Database connection succesfull`))
-//     app.listen(config.PORT, () => {
-//       console.log(colors.cyan.bold(`server running on port ${config.PORT}...`))
+//     app.listen(process.env.PORT, () => {
+//       console.log(colors.cyan.bold(`server running on port ${process.env.PORT}...`))
 //     })
 //   })
 //   .catch((err) => {
 //     console.log(colors.red.bold(`Mongo DB Error ${err}`))
 //     console.log(colors.red.bold(`Mongo DB Error Message ${err.message}`))
 //   })
-const port = process.env.PORT || 8080
-app.listen(8080, () => {
-  console.log(colors.cyan.bold(`server running on port ${port}...`))
-})
+// const port = process.env.PORT || 8080
+// app.listen(8080, () => {
+//   console.log(colors.cyan.bold(`server running on port ${port}...`))
+// })
